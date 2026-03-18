0ab4714158486a1448ddfcac4507c446866aee69
Regina George <reginageo22@gmail.com>
2026-03-18 19:05:53 +0800

    Revert "feat: implement pvp core flow with Riverpod state machine and tests"
    This reverts commit bb09ee1a54800181e1fe01a5a41662e1b0b574f5.

--- apps/mobile/lib/features/pvp/data/repositories/battle_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:yudha_mobile/features/pvp/domain/entities/battle_session_seed.dart';
-
-abstract class BattleRepository {
-  const BattleRepository();
-
-  Future<BattleSessionSeed> createSession();
-}
--- apps/mobile/lib/features/pvp/domain/entities/battle_enums.dart
+++
@@ -1,7 +0,0 @@
-enum BattleMode { bot, online }
-
-enum BattlePhase { preBattle, inBattle, finished }
-
-enum BattleOutcome { inProgress, win, lose, draw }
-
-enum QuestionEffect { damage, heal }
