b66fa49d0496e252740d2f54e9adfb76a0bc516d
Regina George <reginageo22@gmail.com>
2026-06-02 16:12:37 +0800

    feat: improve profile page UI and persist settings

--- apps/mobile/lib/features/profile/application/profile_settings_providers.dart
+++ apps/mobile/lib/features/profile/application/profile_settings_providers.dart
@@ -1,9 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yudha_mobile/features/profile/application/profile_settings_controller.dart';
+import 'package:yudha_mobile/features/profile/application/profile_settings_storage.dart';
 import 'package:yudha_mobile/features/profile/domain/entities/profile_settings.dart';
+
+final Provider<ProfileSettingsStorage> profileSettingsStorageProvider =
+    Provider<ProfileSettingsStorage>(
+      (Ref ref) => const SharedPreferencesProfileSettingsStorage(),
+    );
 
 final StateNotifierProvider<ProfileSettingsController, ProfileSettings>
 profileSettingsProvider =
     StateNotifierProvider<ProfileSettingsController, ProfileSettings>(
-      (Ref ref) => ProfileSettingsController(),
+      (Ref ref) => ProfileSettingsController(
+        storage: ref.watch(profileSettingsStorageProvider),
+      ),
     );
