fd93331770e28680516b551f5f0eedddbece5537
galnoel <galnoelpeterrindengan@gmail.com>
2026-06-03 15:04:51 +0800

    fix: untrack the generated js files

--- apps/backend-api/dist/supabase/supabase.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { SupabaseClient } from '@supabase/supabase-js';
-export declare class SupabaseService {
-    private configService;
-    private readonly logger;
-    private supabase;
-    constructor(configService: ConfigService);
-    getClient(): SupabaseClient;
-}
